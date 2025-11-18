441b723ecebfeee05bc732531c59c6f16d44950e
flaviajaqueline <flaviajaqueline628@gmail.com>
2025-11-17 21:49:36 -0300

    eletrotech funcionando

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8">
 <h1>ELETROTECH - Sistema de Estoque</h1>
 
-<a href="fornecedor/index.php">Gerenciar Fornecedores</a><br><br>
-<a href="categoria/index.php">Gerenciar Categorias</a><br><br>
-<a href="produto/index.php">Gerenciar Produtos</a><br><br>
+<a href="fornecedor/index.php">Gerenciar os Fornecedores</a><br><br>
+<a href="categoria/index.php">Gerenciar as Categorias</a><br><br>
+<a href="produto/index.php">Gerenciar os Produtos</a><br><br>
 <a href="movimentacao/index.php">Movimentações de Estoque</a><br><br>
